0947eeca3aface60bcd15ac1ca681fd7b8c51adf
Michael Owens <michaelowensnl@gmail.com>
2012-06-29 01:10:09 +0200

    Config json file

--- bot.js
+++ bot.js
@@ -6,25 +6,5 @@
  */
 var mssgs = require('./mssgs/api');
 
-/**
- * Configure the bot
- * (soon to become a json file)
- */
-var c = {};
-
-// Bot config
-c.username = 'MikeBot';
-c.commandPrefix = '@' + c.username;
-
-// Web interface config
-c.webPort = 8080;
-c.webPassword = '';
-	
-// debug config
-c.debug = true;
-
-/**
- * Start the bot
- */
-var bot = new mssgs.instance(c);
+var bot = new mssgs.instance();
 bot.connect();
--- mssgs/api.js
+++ mssgs/api.js
@@ -4,18 +4,34 @@
  * A NodeJS plugin-based bot for mss.gs
  * Copyright 2012 Michael Owens & Dean Ward
  */
-var util = require('util'),
-	net = require('net'),
+var fs = require('fs'),
 	user = require('./user'),
-	conversation = require('./conversation');
+	channel = require('./channel');
 
-var instance = exports.instance  = function (config) {
-	this.init(config);
+var instance = exports.instance  = function () {
+	var self = this;
+	fs.readFile('./config.json', 'utf8', function (err, config) {
+		if (err) {
+			console.log('Could not find the config');
+			return;
+		}
+		
+		var c;
+		try {
+			var c = JSON.parse(config);
+			self.init(c);
+		}
+		catch (e) {
+			console.log('Config file seems to be invalid');
+		}
+	});
 };
 
 instance.prototype.init = function (config) {
-	this.username = config.username || 'MikeBot';
-	this.conversations = config.conversations || [];
+	this.username = config.username || 'NodeBot';
+	this.webPort = config.webPort || 8080;
+	this.webPassword = config.webPassword || '';
+	this.channels = config.lastChannels || [];
 };
 
 instance.prototype.connect = function () {
