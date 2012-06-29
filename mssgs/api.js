962bb10f1d6847609719500507484db3b97e82e7
Michael Owens <michaelowensnl@gmail.com>
2012-06-29 03:07:20 +0200

    Some basic message handling and placeholders

--- mssgs/api.js
+++ mssgs/api.js
@@ -30,13 +30,18 @@ var instance = exports.instance  = function () {
 
 instance.prototype.init = function (config) {
 	this.s = null;
-	this.username = config.username || 'NodeBot';
-	this.webPort = config.webPort || 8080;
-	this.webPassword = config.webPassword || '';
-	this.channels = config.lastChannels || [];
+	this.config = {};
+	this.config.username = config.username || 'NodeBot';
+	this.config.avatar = config.avatar || '';
+	this.config.commandPrefix = (!config.commandPrefix || config.commandPrefix.length == 0) ? '@' + this.config.username : config.commandPrefix;
+	this.config.webPort = config.webPort || 8080;
+	this.config.webPassword = config.webPassword || '';
+	this.config.channels = config.lastChannels || [];
+	
+	console.log(this.config.commandPrefix);
 	
 	// web interface
-	this.web = webInterface.init(this.webPort, this.webPassword);
+	this.web = webInterface.init(this.config.webPort, this.config.webPassword);
 	
 	this.connect();
 };
@@ -48,16 +53,40 @@ instance.prototype.connect = function () {
 	this.s = io.connect('api.mss.gs', {port: 443, secure: true, reconnect: true});
 	this.s.on('connect', function () { self.onConnect() });
 	this.s.on('auth', function () { self.onAuth() });
+	this.s.on('message', function (data) { self.onMessage(data) });
 };
 
 instance.prototype.onConnect = function () {
 	console.log('[bot] Connected');
-	this.s.emit('auth', {'username': this.username, 'avatar': 'http://newleafdigital.com/files/nodejs-dark.png'});
+	this.s.emit('auth', {'username': this.config.username, 'avatar': this.config.avatar});
 };
 
 instance.prototype.onAuth = function () {
 	console.log('[bot] Authenticated');
 	this.join('nodebot');
+};
+
+instance.prototype.onMessage = function (data) {
+	console.log('[bot][<<]: Message from ' + data.username);
+	
+	if (data.username.toLowerCase() == 'internal') {
+		// handle internal
+		return;
+	}
+	
+	var arg = data.message.split(' ');
+	if (arg[0].toLowerCase() !== this.config.commandPrefix.toLowerCase()) return;
+	
+	arg.shift();
+	var cmd = arg[0];
+	arg.shift();
+	var msg = arg.join(' ');
+	console.log('[command]: ' + cmd);
+	console.log('[arguments]:');
+	console.log(arg);
+	console.log('[message]: ' + msg);
+	
+	this.s.emit('message', {'conversation': data.conversation, 'text': 'What\'s up, ' + data.username + '?'});
 };
 
 instance.prototype.join = function (channel, password) {
