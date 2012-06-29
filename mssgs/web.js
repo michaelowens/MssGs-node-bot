a032e71ae14fe1437a55d5b273a3f8ef912a460c
Michael Owens <michaelowensnl@gmail.com>
2012-06-29 02:45:56 +0200

    Fixed express

--- mssgs/web.js
+++ mssgs/web.js
@@ -11,23 +11,24 @@ exports.init = web = function (port, password) {
 	this.password = password;
 	
 	this.app = app = express.createServer();
-	app.get('/', function(req, res) {
-		res.render('index.ejs');
-	});
 	
 	app.configure(function () {
 		app.use(express.methodOverride());
 	    app.use(express.bodyParser());
 	    app.use(app.router);
 		//app.use(express.static(__dirname + '/../web_ui/assets', {maxAge: 31557600000})); // one year
-		app.use(express.static(__dirname + '/web_ui'));
+		app.use(express.static(__dirname + '/../web_ui/assets'));
 		
-		app.set('views', __dirname + '/web_ui');
+		app.set('views', __dirname + '/../web_ui');
 		app.set('view engine', 'ejs');
 		app.set('view options', {
 			open: '{{',
 			close: '}}'
 		});
+	});
+	
+	app.get('/', function(req, res) {
+		res.render('index');
 	});
 	
 	app.listen(port);
