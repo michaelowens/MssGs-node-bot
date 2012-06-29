09dc5bedccabcb4fea297612f905f96217087363
Michael Owens <michaelowensnl@gmail.com>
2012-06-29 02:16:34 +0200

    Connection set up with some basic callbacks

--- mssgs/api.js
+++ mssgs/api.js
@@ -5,6 +5,7 @@
  * Copyright 2012 Michael Owens & Dean Ward
  */
 var fs = require('fs'),
+	io = require('socket.io-client'),
 	webInterface = require('./web'),
 	user = require('./user'),
 	channel = require('./channel');
@@ -19,15 +20,16 @@ var instance = exports.instance  = function () {
 		var c;
 		try {
 			var c = JSON.parse(config);
-			self.init(c);
 		}
 		catch (e) {
-			throw 'Config file seems to be invalid';
+			throw '[error] ' + e.message;
 		}
+		self.init(c);
 	});
 };
 
 instance.prototype.init = function (config) {
+	this.s = null;
 	this.username = config.username || 'NodeBot';
 	this.webPort = config.webPort || 8080;
 	this.webPassword = config.webPassword || '';
@@ -41,4 +43,27 @@ instance.prototype.init = function (config) {
 
 instance.prototype.connect = function () {
 	console.log('[bot] Start...');
+	
+	var self = this;
+	this.s = io.connect('api.mss.gs', {port: 443, secure: true, reconnect: true});
+	this.s.on('connect', function () { self.onConnect() });
+	this.s.on('auth', function () { self.onAuth() });
+};
+
+instance.prototype.onConnect = function () {
+	console.log('[bot] Connected');
+	this.s.emit('auth', {'username': this.username, 'avatar': 'http://newleafdigital.com/files/nodejs-dark.png'});
+};
+
+instance.prototype.onAuth = function () {
+	console.log('[bot] Authenticated');
+	this.join('nodebot');
+};
+
+instance.prototype.join = function (channel, password) {
+	console.log('[bot] Joining channel: ' + channel);
+	var packet = {'conversation': channel};
+	if (password !== null) packet['robot password'] = password;
+	
+	this.s.emit('join conversation', packet);
 };
