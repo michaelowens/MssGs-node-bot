e3ec629c0fd810b0cd5106c727164b9f25f95229
Michael Owens <michaelowensnl@gmail.com>
2012-06-29 02:39:23 +0200

    Express installed and failing

--- mssgs/web.js
+++ mssgs/web.js
@@ -4,9 +4,33 @@
  * A NodeJS plugin-based bot for mss.gs
  * Copyright 2012 Michael Owens & Dean Ward
  */
-var web = exports.init = function (port, password) {
+var express = require('express');
+
+exports.init = web = function (port, password) {
 	this.port = port;
 	this.password = password;
+	
+	this.app = app = express.createServer();
+	app.get('/', function(req, res) {
+		res.render('index.ejs');
+	});
+	
+	app.configure(function () {
+		app.use(express.methodOverride());
+	    app.use(express.bodyParser());
+	    app.use(app.router);
+		//app.use(express.static(__dirname + '/../web_ui/assets', {maxAge: 31557600000})); // one year
+		app.use(express.static(__dirname + '/web_ui'));
+		
+		app.set('views', __dirname + '/web_ui');
+		app.set('view engine', 'ejs');
+		app.set('view options', {
+			open: '{{',
+			close: '}}'
+		});
+	});
+	
+	app.listen(port);
 	
 	console.log('[web] Web interface started on port ' + port);
 };
